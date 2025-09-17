e1d2c7ee5c77e07d53f3bd675d93811448ec154b
Denisthon Mamani Mamani <denmamanim@upt.pe>
2025-09-17 15:07:07 -0500

    Añadimos el stub inicial en main.cpp (estructura del simulador)

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Simulador SO - Unidad 01\n";
+    cout << "Por ahora este es un stub. En siguientes pasos implementaremos:\n";
+    cout << " - Lectura de config/entrada.json (nlohmann/json)\n";
+    cout << " - Planificadores: FCFS, SPN, Round Robin\n";
+    cout << " - Gestión de memoria: First-Fit, Best-Fit\n";
+    return 0;
+}
